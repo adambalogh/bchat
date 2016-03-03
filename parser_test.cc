afaa74c3735e3761e088f475050f50d415fc96d6
Adam Balogh <balogh.adam@icloud.com>
2016-03-03 23:09:13 +0000

    updated tests - parser no longer owns sinked buf

--- parser_test.cc
+++ parser_test.cc
@@ -8,16 +8,16 @@ TEST(Parser, Empty) {
 }
 
 TEST(Parser, ZeroMsgLength) {
-  uint8_t* buf = new uint8_t[4]{0, 0, 0, 0};
+  uint8_t buf[4] = {0, 0, 0, 0};
 
   Parser p;
   p.Sink(buf, 4);
 
-  // Decide what to do in this case
+  // Decide what to expect in this case
 }
 
 TEST(Parser, Length) {
-  uint8_t* buf = new uint8_t[4]{0, 0, 0, 10};
+  uint8_t buf[4]{0, 0, 0, 10};
 
   Parser p;
   p.Sink(buf, 4);
@@ -26,27 +26,22 @@ TEST(Parser, Length) {
 }
 
 TEST(Parser, LengthChunks) {
+  uint8_t buf[5]{0, 0, 0, 1, 1};
   Parser p;
 
-  p.Sink(new uint8_t{0}, 1);
-
+  p.Sink(buf, 1);
   EXPECT_FALSE(p.HasMessages());
 
-  uint8_t* buf = new uint8_t[2];
-  buf[0] = 0;
-  buf[1] = 0;
-  p.Sink(buf, 2);
-
+  p.Sink(buf + 1, 2);
   EXPECT_FALSE(p.HasMessages());
 
-  p.Sink(new uint8_t{1}, 1);
-  p.Sink(new uint8_t{10}, 1);
-
+  p.Sink(buf + 3, 1);
+  p.Sink(buf + 4, 1);
   EXPECT_TRUE(p.HasMessages());
 }
 
 TEST(Parser, Message) {
-  uint8_t* buf = new uint8_t[6]{0, 0, 0, 2, 1, 6};
+  uint8_t buf[6]{0, 0, 0, 2, 1, 6};
 
   Parser p;
   p.Sink(buf, 6);
@@ -61,7 +56,7 @@ TEST(Parser, Message) {
 }
 
 TEST(Parser, TwoMessages) {
-  uint8_t* buf = new uint8_t[15]{
+  uint8_t buf[15]{
       0, 0, 0,  3,  // header #2
       5, 3, 10,     // msg #1
       0, 0, 0,  4,  // header #2
@@ -84,11 +79,11 @@ TEST(Parser, TwoMessages) {
 }
 
 TEST(Parser, Split) {
-  uint8_t* buf = new uint8_t[3]{0, 0, 0};
-  uint8_t* buf2 = new uint8_t[4]{2, 11, 10, 0};
-  uint8_t* buf3 = new uint8_t[1]{0};
-  uint8_t* buf4 = new uint8_t[5]{0, 4, 1, 2, 3};
-  uint8_t* buf5 = new uint8_t[3]{4, 0, 0};
+  uint8_t buf[3]{0, 0, 0};
+  uint8_t buf2[4]{2, 11, 10, 0};
+  uint8_t buf3[1]{0};
+  uint8_t buf4[5]{0, 4, 1, 2, 3};
+  uint8_t buf5[3]{4, 0, 0};
 
   Parser p;
   p.Sink(buf, 3);
@@ -97,6 +92,7 @@ TEST(Parser, Split) {
   auto msg1 = std::vector<uint8_t>{11, 10};
   EXPECT_TRUE(p.HasMessages());
   auto messages = p.GetMessages();
+
   EXPECT_EQ(1, messages.size());
   EXPECT_EQ(msg1, *messages[0]);
   EXPECT_FALSE(p.HasMessages());
@@ -110,6 +106,7 @@ TEST(Parser, Split) {
   p.Sink(buf5, 3);
   auto msg2 = std::vector<uint8_t>{1, 2, 3, 4};
   EXPECT_TRUE(p.HasMessages());
+
   messages = p.GetMessages();
   EXPECT_EQ(msg2, *messages[0]);
   EXPECT_FALSE(p.HasMessages());
