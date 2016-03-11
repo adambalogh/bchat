beeb21b0bbc6fee79c048e402952dcc37e79639a
Adam Balogh <balogh.adam@icloud.com>
2016-03-11 17:18:46 +0000

    user test

---
+++ src/user_test.cc
@@ -0,0 +1,98 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "user.h"
+#include "conn.h"
+#include "proto/message.pb.h"
+
+using namespace ::testing;
+
+const std::string name = "Adam";
+
+class MockConn : public Conn {
+ public:
+  // GMock cannot handle non-copyable parameters
+  void Send(const MessagePtr msg) { SendProxy(msg.get()); }
+
+  MOCK_METHOD2(OnRead, void(ssize_t, const uv_buf_t*));
+  MOCK_METHOD1(SendProxy, void(const Message*));
+};
+
+Conn::MessagePtr MakeMsg(std::string s) {
+  auto msg = std::make_unique<Conn::Message>(s.size());
+  std::copy(s.begin(), s.end(), msg->begin());
+  return std::move(msg);
+}
+
+TEST(UserRepo, Empty) {
+  UserRepo repo;
+  EXPECT_FALSE(repo.Contains(""));
+  EXPECT_FALSE(repo.Contains("a"));
+  EXPECT_NO_THROW(repo.Remove(""));
+}
+
+TEST(User, InvalidRequest) {
+  MockConn conn;
+  UserRepo repo;
+  User u{conn, repo};
+
+  proto::Response res;
+  res.set_type(res.Error);
+  res.mutable_error()->set_type(proto::Error_Type_InvalidRequest);
+  auto bin = MakeMsg(res.SerializeAsString());
+
+  EXPECT_CALL(conn, SendProxy(Pointee(*bin)));
+
+  u.OnMessage(MakeMsg("\n"));
+}
+
+TEST(User, MustAuthenticateFirst) {
+  MockConn conn;
+  UserRepo repo;
+  User u{conn, repo};
+
+  proto::Response res;
+  res.set_type(res.Error);
+  res.mutable_error()->set_type(proto::Error_Type_MustAuthenticateFirst);
+  auto bin = MakeMsg(res.SerializeAsString());
+
+  EXPECT_CALL(conn, SendProxy(Pointee(*bin)));
+
+  proto::Request req;
+  req.set_type(req.Message);
+  u.OnMessage(MakeMsg(req.SerializeAsString()));
+}
+
+TEST(User, MessageSend) {
+  MockConn conn;
+  UserRepo repo;
+  User u{conn, repo};
+
+  proto::Request req;
+  req.set_type(req.Authentication);
+  req.mutable_authentication()->set_name("Adam");
+  u.OnMessage(MakeMsg(req.SerializeAsString()));
+
+  proto::Message message;
+  message.set_content("Hello!");
+  message.set_recipient("Adam");
+
+  proto::Response res;
+  res.set_type(res.Message);
+  *res.mutable_message() = message;
+  res.mutable_message()->set_sender("Adam");
+  auto bin = MakeMsg(res.SerializeAsString());
+
+  EXPECT_CALL(conn, SendProxy(Pointee(*bin)));
+
+  req.Clear();
+
+  req.set_type(req.Message);
+  *req.mutable_message() = message;
+  u.OnMessage(MakeMsg(req.SerializeAsString()));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
